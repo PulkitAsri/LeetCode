34cdcbdb2d687a3da0003ffdf259290d8a124680
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-01-20 21:41:08 +0530

    Time: 48 ms (64.78%), Space: 18.8 MB (85.48%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,8 +1,8 @@
 #define ll long long
 class Solution {
     
-    bool checkBananas(vector<int> piles, ll h,ll k){
-        ll count=0;
+    bool checkBananas(vector<int> &piles, int h,int k){
+        int count=0;
         for(int x: piles ){
             count+= (x%k ==0)? x/k: (x/k)+1;
         }
@@ -13,15 +13,18 @@ public:
         
         int n=piles.size();
         
-        ll l=0;
-        ll r=1e9;
+        int mx= piles[0];
+        for(int x:piles) mx=max(mx,x);
+        
+        int l=0;
+        int r=mx+1;
         
         //Binary Search between 0, 10^9 
         // => log(10^9) => 29.89 
         // => max 30 times
         
         while(r-l > 1){
-            ll m =(l+r)/2;
+            int m =(l+r)/2;
             
             if(checkBananas(piles,h,m))
                 r=m;
