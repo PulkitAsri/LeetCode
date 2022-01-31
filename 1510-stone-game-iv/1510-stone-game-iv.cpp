425ea48eb6dc3b3acb47f99442268cfaf42eb77b
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-01-31 23:03:16 +0530

    Time: 352 ms (12.94%), Space: 44.1 MB (5.00%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<bool> winnerAt;
+public:
+    
+    bool winnerSquareGame(int n){
+        vector<int> dp(100001,-1);
+        return winnerSquareGame(dp, n);
+    }
+    
+    bool winnerSquareGame(vector<int> &dp, int n) {
+        
+        if(n<=0) return false;
+        if(dp[n]!=-1) return dp[n];
+        
+        bool ans=false;
+        
+        for(int i=1; i*i<=n; i++){
+            if(!winnerSquareGame(dp, n - i*i)){
+                ans=true;
+            }
+        }
+        return dp[n]=ans;
+        
+    }
+};
