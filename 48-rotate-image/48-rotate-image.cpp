c5d93f09c00f1c997dc25b0f1a30c8676be84b50
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-02-02 03:31:17 +0530

    Time: 4 ms (50.58%), Space: 7.2 MB (33.49%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,77 @@
+class Solution {
+    
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int dx[]={0,1,0,-1};
+        int dy[]={1,0,-1,0};
+        
+        int n=matrix.size();
+        
+        //for Each Layer
+        
+        
+        
+        for(int j=0; j< n/2; j++){
+            int X[]={j,j,n-1-j,n-1-j};
+            int Y[]={j,n-1-j,n-1-j,j};
+            
+            for(int q=0; q < n-2*j-1 ; q++){
+                
+                int o=X[0] + q*dx[0];
+                int p=Y[0] + q*dy[0];
+                // cout<<o<<" "<<p<<endl;
+                int o1,p1;
+                
+                int temp=matrix[o][p];
+                for(int i=3; i>0; i--){
+                    o1=X[i] + q*dx[i];
+                    p1=Y[i] + q*dy[i];
+                    
+                    // cout<<o<<" "<<p<<"<="<<o1<<" "<<p1<<endl;
+                    
+                    matrix[o][p]=matrix[o1][p1];
+                    o=o1;
+                    p=p1;  
+                }
+                // cout<<o<<" "<<p<<endl;
+                matrix[o][p]=temp;
+                
+                // cout<<"======================"<<endl;
+            }
+            // cout<<"======================"<<endl;
+        }  
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
