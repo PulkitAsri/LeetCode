094d617de24b16a2f17b3db1c9e8fce350ceb15c
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-02-21 11:20:15 +0530

    Time: 27 ms (50.68%), Space: 19.6 MB (88.96%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,33 @@
 class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
+    
+    int freqTable(vector<int>& nums) {
         int n=nums.size();
         map<int,int> freq;
-        for(int x: nums){
+        for(int &x: nums){
             freq[x]++;
             if(freq[x]== ceil(n/2.0)) return x;
         }
         return 0;
+    }
+    
+    int mooreVoting(vector<int>& nums){
+        int n=nums.size();
+        int count=0;
+        int currCandidate=-1;
+        for(int &x: nums){
+            if(count==0){
+                currCandidate=x;
+                count++; 
+            }
+            else if(currCandidate==x) count++;
+            else count--;
+        }
+        return currCandidate;
+    }
+    
+public:
+    int majorityElement(vector<int>& nums) {
+        // return freqTable(nums);
+        return mooreVoting(nums);
     }
 };
