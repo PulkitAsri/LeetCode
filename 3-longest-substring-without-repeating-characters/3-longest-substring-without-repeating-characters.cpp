bb4af37e94485a74c384f500b22a45f20d033f79
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-06-10 13:29:59 +0530

    Time: 95 ms (14.50%), Space: 15 MB (11.97%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,37 +5,49 @@ class Solution {
         }
         return true;
     }
+    bool isGoodSubstring(string &s, int &w){
+        map<char,int> mp;
+        int n=s.length();
+        
+        bool flag=false;
+        if(w<=1) return true;
+        for(int i=0; i<w; i++) mp[s[i]]++;
+        if(isGood(mp)){ 
+            flag=true;
+        }
+        // cout<<s.substr(0,w)<<" "<<endl;
+        for(int i=1; i+w <= n; i++){
+
+            mp[s[i-1]]--;
+            mp[s[i+w-1]]++;
+            bool b=isGood(mp);
+            // cout<<w<<" "<<s.substr(i,w)<<" "<<b<<endl;;
+            if(b){ 
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+    }
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.length();
         int ans=0;
         int w=min(n,256);
         
-        while(w){
-            map<char,int> mp;
-            bool flag=false;
-            for(int i=0; i<w; i++) mp[s[i]]++;
-            if(isGood(mp)){ 
-                ans=w;
-                flag=true;
-                break;
+        
+        int l=0;
+        int r=w+1;
+        
+        while(r-l > 1){
+            int m= (l+r)/2;
+            if(isGoodSubstring(s,m)){
+                l=m;
+            }else{
+                r=m;
             }
-            // cout<<s.substr(0,w)<<" "<<endl;
-            for(int i=1; i+w <= n; i++){
-                
-                mp[s[i-1]]--;
-                mp[s[i+w-1]]++;
-                bool b=isGood(mp);
-                // cout<<w<<" "<<s.substr(i,w)<<" "<<b<<endl;;
-                if(b){ 
-                    ans=w;
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag) return ans;
-            w--;
         }
-        return ans;
+        
+        return l;
     }
 };
