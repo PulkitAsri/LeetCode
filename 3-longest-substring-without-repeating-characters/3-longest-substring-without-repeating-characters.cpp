6ef076f5c219c48e09e6190805a8e5c4564d8c52
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-06-10 12:58:56 +0530

    Time: 1485 ms (5.00%), Space: 84.2 MB (9.12%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,41 @@
 class Solution {
+    bool isGood(map<char,int> &mp){
+        for(auto &p: mp){
+            if ( p.second > 1 ) return false;
+        }
+        return true;
+    }
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        unordered_set<char> set;
-        
         int n=s.length();
         int ans=0;
-        int st=0,e=0;
-        while(e<n){
-            
-            if(set.count(s[e])==0){
-                set.insert(s[e]);
-                e++;           
-            }else{
-                set.erase(s[st]);
-                ++st;
+        int w=min(n,256);
+        
+        while(w){
+            map<char,int> mp;
+            bool flag=false;
+            for(int i=0; i<w; i++) mp[s[i]]++;
+            if(isGood(mp)){ 
+                ans=w;
+                flag=true;
+                break;
             }
-            
-            ans=max(ans, e-st);
+            // cout<<s.substr(0,w)<<" "<<endl;
+            for(int i=1; i+w <= n; i++){
+                
+                mp[s[i-1]]--;
+                mp[s[i+w-1]]++;
+                bool b=isGood(mp);
+                // cout<<w<<" "<<s.substr(i,w)<<" "<<b<<endl;;
+                if(b){ 
+                    ans=w;
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag) return ans;
+            w--;
         }
-        return ans;     
+        return ans;
     }
-    
 };
