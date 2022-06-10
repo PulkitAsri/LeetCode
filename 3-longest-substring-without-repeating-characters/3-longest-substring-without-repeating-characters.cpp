8c9d088172d05a8ae2939720ef612a917b0ee83c
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-06-11 00:20:29 +0530

    Time: 6 ms (93.03%), Space: 7.7 MB (81.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,4 +1,6 @@
 class Solution {
+    
+    //auxiliary methods 
     bool isGood(map<char,int> &mp){
         for(auto &p: mp){
             if ( p.second > 1 ) return false;
@@ -29,12 +31,46 @@ class Solution {
         }
         return flag;
     }
-public:
-    int lengthOfLongestSubstring(string s) {
+    
+    
+    //approaches
+    //O(n*256)
+    int windowingWordLengths(string s) {
         int n=s.length();
         int ans=0;
         int w=min(n,256);
         
+        while(w){
+            map<char,int> mp;
+            bool flag=false;
+            for(int i=0; i<w; i++) mp[s[i]]++;
+            if(isGood(mp)){ 
+                ans=w;
+                flag=true;
+                break;
+            }
+            for(int i=1; i+w <= n; i++){
+                
+                mp[s[i-1]]--;
+                mp[s[i+w-1]]++;
+                bool b=isGood(mp);
+                if(b){ 
+                    ans=w;
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag) return ans;
+            w--;
+        }
+        return ans;
+    }
+    
+    //O(nLog(256))
+    int windowingWithBinarySearch(string s) {
+        int n=s.length();
+        int ans=0;
+        int w=min(n,256);
         
         int l=0;
         int r=w+1;
@@ -47,7 +83,34 @@ public:
                 r=m;
             }
         }
-        
+        //Queue + map-> O(n)
         return l;
+    }
+    
+    
+public:
+    int lengthOfLongestSubstring(string s) {
+        // return windowingWordLengths(s);
+        // return windowingWithBinarySearch(s);
+        
+        int n=s.length();
+        
+        queue<char> q;
+        vector<bool> mp(256,false);
+        int ans=0;
+        for(char &c: s){
+            if(mp[c]==true){
+                while(q.front()!=c) {
+                    mp[q.front()]=false;
+                    q.pop();
+                }
+                mp[q.front()]=false;
+                q.pop();
+            }
+            mp[c]=true;
+            q.push(c);
+            ans= max((int)q.size(), ans);
+        }
+        return ans;
     }
 };
