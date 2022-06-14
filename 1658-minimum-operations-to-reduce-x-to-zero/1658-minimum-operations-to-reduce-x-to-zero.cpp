f2d65ca753079674ee9935502f1a77ca23a38c9d
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-06-14 08:59:08 +0530

    Time: 950 ms (7.99%), Space: 177 MB (6.35%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -5,6 +5,10 @@ public:
         int n=nums.size();
         vector<int> prefix(n,0);
         vector<int> suffix(n,0);
+        
+        //map implementation
+        unordered_map<int,int> mp;
+        
         int ans=INT_MAX;
         int p=0;
         for(int i=0; i<n; i++){
@@ -13,6 +17,7 @@ public:
             if(prefix[i] == x){
                 ans=min(ans, i+1);
             }
+            mp[prefix[i]]=i;  
         }
         int s=0;
         for(int i=n-1; i>=0; i--){
@@ -33,17 +38,21 @@ public:
             //search the number betwen 0<----> (i-1)
             // but 0 has no need coz it wont have any numbers to search from and also (prefix.begin() -1) doesnt work
             
+            //binary search 
+            // auto itr = lower_bound(prefix.begin(), prefix.begin()+ i-1  , numberNeeded);
+            // int idx=  (int)(itr - prefix.begin());
+            // if(*itr == numberNeeded ){
+            //     ans= min(ans, n-i + idx+1 );
+            //     // cout<<(n-i)<<" "<<idx<<endl;
+            // }
             
-            auto itr = lower_bound(prefix.begin(), prefix.begin()+ i-1  , numberNeeded);
-            // cout<< *(prefix.begin() + -1) <<endl;
-            // cout<< suffix[i]<<" "<< numberNeeded<<" "<< *itr<<"=> "<<endl;
             
-            int idx=  (int)(itr - prefix.begin());
-            
-            if(*itr == numberNeeded ){
-                ans= min(ans, n-i + idx+1 );
-                // cout<<(n-i)<<" "<<idx<<endl;
+            //map
+            if(mp.count(numberNeeded)!=0 && mp[numberNeeded] < i){
+                ans= min(ans, n-i + mp[numberNeeded]+1 );
             }
+            
+            
         }
         
         return (ans==INT_MAX)? -1: ans;
