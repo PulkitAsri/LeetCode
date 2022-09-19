1921b678ae367dacbf413f2cf115dc1dff95803a
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-09-19 23:48:00 +0530

    Time: 2102 ms (5.00%), Space: 88.5 MB (14.46%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,42 +1,30 @@
+int dp[202][100005];
 class Solution {
-
-    bool subsetSum(vector<int> &nums, int sum){
+    bool canPartition(vector<int> &nums, int idx, int sum){
+        if(sum==0) return true;
+        if(idx==0) return false;
         
-        int n=nums.size();
+        if(dp[idx][sum]!=-1) return dp[idx][sum];
         
-        bool dp[202][200002];
+        if(nums[idx-1] <= sum){
+            return dp[idx][sum]= canPartition(nums,idx-1,sum) or canPartition(nums,idx-1,sum-nums[idx-1]);
+        }else return dp[idx][sum]= canPartition(nums, idx-1, sum);
         
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=sum; j++){
-                if(i==0) dp[i][j]= false;
-                if(j==0) dp[i][j]=true;
-            }
-        }
-        
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=sum; j++){
-                if( nums[i-1] <= j ){
-                    //nums[i-1]=> w1 ,  j=>W
-                    dp[i][j]= (dp[i-1][j] or dp[i-1][j-nums[i-1]]);
-                }else{
-                    dp[i][j]= dp[i-1][j];
-                }
-            }
-        }
-        
-        return dp[n][sum];
         
     }
 public:
     bool canPartition(vector<int>& nums) {
-        long long sum=0;
-        for(int &x:nums) sum+=x;
-        
-        if(sum%2==1){
-            return false;
-        }else{
-            return subsetSum(nums, sum/2);
-        }
+        memset(dp,-1, sizeof dp);
+        int sum = 0; 
+        int n= nums.size();
+        for(int x: nums) sum+=x;
+        if(sum%2==1) return false;
+        return canPartition(nums, n, sum/2);
     }
 };
+
+  // 0. 1. 2. 3. 4. 5  
+// 0 T
+// 1
+// 2
+// 3
