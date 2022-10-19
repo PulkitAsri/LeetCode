f1c2f638909815873b6b3ac5319660c86fc2b12d
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-10-20 01:13:00 +0530

    Time: 61 ms (78.39%), Space: 14.6 MB (54.44%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,43 +1,43 @@
 int dp[2002][2002];
 class Solution {
-public:
-    bool solve(int i, int j, string &s, string &t){
-        
-        int &ans = dp[i+1][j+1];
-        if(ans!=-1) return ans;
-        
+  public:
+    bool solve(int i, int j, string & s, string & t) {
+
+      int & ans = dp[i + 1][j + 1];
+      if (ans != -1) return ans;
+
+      ans = false;
+
+      if (i < 0 and j < 0)
+        ans = true;
+      else if (j < 0)
         ans = false;
-    
-        if(i<0 and j<0) 
-            ans= true;
-        else if(j<0) 
-            ans= false;
-        else if(i<0) 
-            ans = t[j] =='*' && solve(i,j-1,s,t);
-        else if(s[i]==t[j]){
-            //match
-            ans= solve(i-1,j-1,s,t);
-        }else{
-            if(t[j]=='*'){
-                //either process it and stay or exit the star
-                ans = solve(i-1,j,s,t) or solve(i,j-1,s,t);
-            }else if (t[j]=='?'){
-                ans = solve(i-1,j-1,s,t);
-            }
+      else if (i < 0)
+        ans = t[j] == '*' && solve(i, j - 1, s, t);
+      else if (s[i] == t[j]) {
+        //match
+        ans = solve(i - 1, j - 1, s, t);
+      } else {
+        if (t[j] == '*') {
+          //either process it and stay or exit the star
+          ans = solve(i - 1, j, s, t) or solve(i, j - 1, s, t);
+        } else if (t[j] == '?') {
+          ans = solve(i - 1, j - 1, s, t);
         }
-        
-        return ans;
+      }
+
+      return ans;
     }
-    bool isMatch(string s, string t) {
-        int n= s.length();
-        int m= t.length();
-        
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=m; j++){
-                dp[i][j]=-1;
-            }
-        }
-        
-        return solve(n-1,m-1,s,t);
+  bool isMatch(string s, string t) {
+    int n = s.length();
+    int m = t.length();
+
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= m; j++) {
+        dp[i][j] = -1;
+      }
     }
+
+    return solve(n - 1, m - 1, s, t);
+  }
 };
