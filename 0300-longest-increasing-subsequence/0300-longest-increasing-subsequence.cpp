d7f17ca37f92671f6dfa458c1a857c48244c2b0d
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-10-30 01:17:35 +0530

    Time: 2526 ms (6.30%), Space: 10.8 MB (24.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,40 +14,23 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n= nums.size();
-        vector<vector<int>> dp(n+2, vector<int>(n+2,-1));
+        vector<vector<int>> dp(2, vector<int>(n+2,-1));
         // return solve(0,-1,nums,n,dp);
         
         for(int i=n; i>=0; i--){
             for(int pi = n; pi>=0; pi--){
-                if(i==n) dp[i][pi] = 0;
+                if(i==n) dp[i%2][pi] = 0;
                 else if(pi == 0 or ( nums[i] > nums[pi-1])){
-                    int take= 1 + dp[i+1][i+1];
-                    int notTake= dp[i+1][pi];
-                    dp[i][pi]= max(take, notTake);
+                    int take= 1 + dp[(i+1)%2][i+1];
+                    int notTake= dp[(i+1)%2][pi];
+                    dp[i%2][pi]= max(take, notTake);
                 }else{
-                    dp[i][pi]= dp[i+1][pi];
+                    dp[i%2][pi]= dp[(i+1)%2][pi];
                 }
             }
         }
         
         return dp[0][0];
-        
-        
-        
-        
-//         for(int i=0; i<=n; i++){
-//             if(i==n) dp[i][prevIdx]=0;
-//             else if(prevIdx!=-1 && nums[i] > nums[prevIdx]){
-//                 int take= 1 + dp[i-1];
-//                 int notTake= dp[i-1];
-                
-//                 if(take >= notTake){
-//                     prevIdx = i;
-//                     return take;
-//                 }else return notTake;
-//             }else return dp[i-1];
-//         }
-        
-        // return dp[n];
+
     }
 };
