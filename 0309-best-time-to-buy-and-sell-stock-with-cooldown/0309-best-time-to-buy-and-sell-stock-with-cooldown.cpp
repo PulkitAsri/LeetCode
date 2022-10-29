c668d827f235fa3a5372aa05b077cf7ce04c168d
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-10-30 00:31:58 +0530

    Time: 13 ms (28.82%), Space: 11.2 MB (90.84%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,29 +6,24 @@ public:
         int n= prices.size();
         // return solve(0,true,prices,n);
         
-        vector<int> prev2(2,0);
-        vector<int> prev(2,0);
-        vector<int> curr(2,0);
-
+        vector<vector<int>> dp(3, vector<int>(2,0));
 
         for(int i=n+1; i>=0; i--){
             for(int b=0; b<=1; b++){
                 
-                if(i>=n) curr[b]=0;
+                if(i>=n) dp[i%3][b]=0;
                 else if(b){
-                    int buy= prev[!b] - prices[i];
-                    int notBuy= prev[b];
-                    curr[b]= max(buy,notBuy);
+                    int buy= dp[(i+1)%3][!b] - prices[i];
+                    int notBuy= dp[(i+1)%3][b];
+                    dp[i%3][b]= max(buy,notBuy);
                     
                 }else{
-                    int sell= prev2[!b] + prices[i];
-                    int notSell= prev[b];
-                    curr[b]= max(sell,notSell);
+                    int sell= dp[(i+2)%3][!b] + prices[i];
+                    int notSell= dp[(i+1)%3][b];
+                    dp[i%3][b]= max(sell,notSell);
                 }
             }
-            prev2=prev;
-            prev=curr;
         }
-        return prev[true];
+        return dp[0][true];
     }
 };
