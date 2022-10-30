85ba708f0f8ddd2fdd1c109b9b8de9305c5838ce
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-10-30 19:03:31 +0530

    Time: 676 ms (34.42%), Space: 10.6 MB (30.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,23 +14,44 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n= nums.size();
-        vector<vector<int>> dp(2, vector<int>(n+2,-1));
         // return solve(0,-1,nums,n,dp);
         
-        for(int i=n; i>=0; i--){
-            for(int pi = n; pi>=0; pi--){
-                if(i==n) dp[i%2][pi] = 0;
-                else if(pi == 0 or ( nums[i] > nums[pi-1])){
-                    int take= 1 + dp[(i+1)%2][i+1];
-                    int notTake= dp[(i+1)%2][pi];
-                    dp[i%2][pi]= max(take, notTake);
-                }else{
-                    dp[i%2][pi]= dp[(i+1)%2][pi];
+        //ITERATIVE APPROACH FOR RECUSRION
+//         vector<vector<int>> dp(2, vector<int>(n+2,-1));
+
+//         for(int i=n; i>=0; i--){
+//             for(int pi = n; pi>=0; pi--){
+//                 if(i==n) dp[i%2][pi] = 0;
+//                 else if(pi == 0 or ( nums[i] > nums[pi-1])){
+//                     int take= 1 + dp[(i+1)%2][i+1];
+//                     int notTake= dp[(i+1)%2][pi];
+//                     dp[i%2][pi]= max(take, notTake);
+//                 }else{
+//                     dp[i%2][pi]= dp[(i+1)%2][pi];
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+        
+        //ITERATIVE APPRAOCH FOR RECONSTRUCTION
+        vector<int> dp(n, 1); 
+        vector<int> path(n,-1);
+        path[0]=0;
+        int ans=dp[0];
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i]= max(dp[j]+1, dp[i]);
                 }
             }
+            ans= max(ans,dp[i]);
         }
+        return ans;
         
-        return dp[0][0];
 
+        
+        //ITERATIVE APPROACH TIME OPTIMIZATION USING BINARY SEARCH
+        
+        
     }
 };
