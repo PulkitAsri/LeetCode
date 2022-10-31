e69b720763d66b1e8e7dfe5fbb4f268049f3c03d
Pulkit Asri <68493943+PulkitAsri@users.noreply.github.com>
2022-11-01 00:38:13 +0530

    Time: 45 ms (86.82%), Space: 12.4 MB (11.79%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& arr) {
-        int n=arr.size();
+        int n= arr.size();
         vector<int> dp1(n,1);
         vector<int> dp2(n,1);
-
+        
+        vector<int> seq;
         for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(arr[j] < arr[i]){
-                    dp1[i]= max(dp1[i], dp1[j]+1);
-                }
+            int idx= lower_bound(seq.begin(), seq.end(), arr[i]) - seq.begin();
+            dp1[i]= idx+1;
+            if(idx==seq.size()){
+                seq.push_back(arr[i]);   
+            }else{
+                seq[idx]= arr[i];
             }
         }
-
-        for(int i=n-1; i>=0; i--){
-            for(int j=n-1; j>i; j--){
-                if(arr[j] < arr[i]){
-                    dp2[i]= max(dp2[i], dp2[j]+1);
-                }
+        reverse(arr.begin(),arr.end());
+        
+        vector<int> seq2;
+        for(int i=0; i<n; i++){
+            int idx= lower_bound(seq2.begin(), seq2.end(), arr[i]) - seq2.begin();
+            dp2[i]= idx+1;
+            if(idx==seq2.size()){
+                seq2.push_back(arr[i]);   
+            }else{
+                seq2[idx]= arr[i];
             }
         }
+        
+        reverse(dp2.begin(), dp2.end());
+//         for(int i=0; i<n; i++) cout<<dp1[i]<<" "; cout<<endl;
+//         for(int i=0; i<n; i++) cout<<dp2[i]<<" "; cout<<endl;
 
-        // for(int i=0; i<n; i++) cout<<dp1[i]<<" "; cout<<endl;
-        // for(int i=0; i<n; i++) cout<<dp2[i]<<" "; cout<<endl;
-
-        int ans=1;
+        int ans=0;
         for(int i=0; i<n; i++){
-            if(dp1[i]!=1 && dp2[i]!=1)
-            ans= max(ans, dp1[i] + dp2[i] -1);
+            if(dp1[i]!=1 and dp2[i]!=1)
+                ans= max(ans, dp1[i] + dp2[i] - 1);
         }
-
-        return n-ans; 
+        
+        return n- ans; 
     }
 };
